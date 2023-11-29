0936ecdfc01854060de321d63a4ee469ac69e560
erickrapha <erick.r@escolar.ifrn.edu.br>
2023-11-29 11:55:56 -0300

    scriptables

---
+++ sisteDialogo/Assets/SistemaDeDialogo/Scripts/dialogueManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogueManager : MonoBehaviour
+{
+    public EfeitoDigitador caixaDeDialogo;
+    public GameObject botaoContinuar;
+    public bool rodando, proximo;
+    public textList dialogo;
+
+    void Start()
+    {
+       
+    }
+    IEnumerator ExibirSequencia(List<string> lista)
+    {
+        if (rodando)
+        {
+            yield break;
+        }
+        rodando = true;
+        proximo = false;
+        botaoContinuar.SetActive(false);
+        foreach(var mensagem in lista)
+        {
+            caixaDeDialogo.ImprimirMensagem(mensagem);
+            while (caixaDeDialogo.imprimindo)
+            {
+                yield return new WaitForEndOfFrame();
+            }
+            botaoContinuar.SetActive(true);
+            while (!proximo)
+            {
+                yield return new WaitForEndOfFrame();
+            }
+            botaoContinuar.SetActive(false);
+            proximo = false;
+        }
+        caixaDeDialogo.Limpar();
+        rodando = false;
+        StopCoroutine(ExibirSequencia(lista));
+    }
+    void Update()
+    {   
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            StartCoroutine(ExibirSequencia(dialogo.lista1));
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            StartCoroutine(ExibirSequencia(dialogo.lista2));
+        }
+        if (!caixaDeDialogo.imprimindo)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                proximo = true;
+            }
+        }
+    }
+}
---
+++ sisteDialogo/Assets/SistemaDeDialogo/Scripts/textList.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Dialogo", menuName = "ScriptableObjects/Dialogo", order = 0)]
+public class textList : ScriptableObject 
+{
+    public List<string> lista1;
+    public List<string> lista2;
+
+}
